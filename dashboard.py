f47fcf68ddcb5b4f030b74d7589005bd405fc15d
131-hey <m120b4kx2208@bangkit.academy>
2024-10-06 18:47:42 +0700

    Add files via upload

---
+++ dashboard.py
@@ -0,0 +1,40 @@
+import io
+import streamlit as st
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+ 
+st.title('Bike Sharing Dataset')
+tab1, tab2= st.tabs(["Tab 1", "Tab 2"])
+ 
+with tab1:
+    st.header("Gathering Data")
+
+    day_df = pd.read_csv("day.csv")
+    st.dataframe(day_df.head())
+    st.caption('Dengan menggunakan function read_csv() program akan membaca file csv yang kita lampirkan, lalu akan ditampilkan beberapa dengan function head()')
+
+
+    st.header("Assessing Data")
+
+    st.subheader('Info Data')
+    st.write(day_df.info())
+
+ 
+with tab2:
+    st.header("Gathering Data")
+
+    hour_df = pd.read_csv("hour.csv")
+    st.dataframe(hour_df.head())
+    st.caption('Dengan menggunakan function read_csv() program akan membaca file csv yang kita lampirkan, lalu akan ditampilkan beberapa dengan function head()')
+
+    st.header("Assessing Data")
+
+    st.subheader('Info Data')
+    st.write(hour_df.info())
+
+
+
+
+ 
